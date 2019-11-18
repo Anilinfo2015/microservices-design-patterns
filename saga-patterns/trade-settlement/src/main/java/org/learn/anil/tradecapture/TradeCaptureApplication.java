e90950c41287cac4055319d6fec97a9d4309769d
practiceoverflow <practice.overflow@gmail.com>
2019-11-18 20:26:06 +0530

    saga patterns

---
+++ saga-patterns/trade-settlement/src/main/java/org/learn/anil/tradecapture/TradeCaptureApplication.java
@@ -0,0 +1,13 @@
+package org.learn.anil.tradecapture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeCaptureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeCaptureApplication.class, args);
+	}
+
+}
---
+++ saga-patterns/trade-settlement/src/test/java/org/learn/anil/tradecapture/TradeCaptureApplicationTests.java
@@ -0,0 +1,16 @@
+package org.learn.anil.tradecapture;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TradeCaptureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
